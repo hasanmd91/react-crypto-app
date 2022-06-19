4c1ce1340147a173284221f2eadd55e6212edcfb
hasanmd91 <hasanmd1991@hotmail.com>
2022-06-19 22:56:50 +0300

    css added

--- my-app/src/Components/Coins.js
+++ my-app/src/Components/Coins.js
@@ -1,47 +1,30 @@
 import React from "react";
-import "./coins.css";
+import style from "./coins.module.css";
 
-const Coins = (props) => {
+export const Coins = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
   return (
-    <>
-      {props.coinData.map((coin) => {
-        const {
-          id,
-          image,
-          name,
-          symbol,
-          current_price,
-          market_cap,
-          price_change_percentage_24h,
-        } = coin;
-        const priceChangeObject = { price_change_percentage_24h };
-        const pricechange = priceChangeObject.price_change_percentage_24h;
-        return (
-          <div className="coin-row" key={id}>
-            <div className="coins">
-              <img src={image} alt="crypto" />
-              <h1>{name}</h1>
-              <p className="coin-symbol">{symbol}</p>
-            </div>
-            <div className="coin-data"></div>
-            <p className="coin-price"> ${current_price}</p>
-            <p>
-              {pricechange < 0 ? (
-                <span className="coin-percent red">
-                  {pricechange.toFixed(2)}%
-                </span>
-              ) : (
-                <span>{pricechange.toFixed(2)}%</span>
-              )}
-              <p className="coin-voulme ">
-                Mkt cap: ${market_cap.toLocaleString()}
-              </p>
-            </p>
-          </div>
-        );
-      })}
-    </>
+    <div className={style.container}>
+      <div className={style.row}>
+        <img src={image} alt="crypto" />
+        <h1> {name}</h1>
+        <p> {symbol}</p>
+        <p>${price}</p>
+        <p>${volume.toLocaleString()}</p>
+        {priceChange < 0 ? (
+          <p>{priceChange.toFixed(2)}%</p>
+        ) : (
+          <p>{priceChange.toFixed(2)}%</p>
+        )}
+        <p>Mkt Cap: ${marketcap.toLocaleString()}</p>
+      </div>
+    </div>
   );
 };
-
-export default Coins;
--- my-app/src/Components/Crypto.js
+++ my-app/src/Components/Crypto.js
@@ -1,51 +1,51 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import axios from "axios";
-
-import Coins from "./Coins";
-import Search from "./Search";
+import { Coins } from "./Coins";
 
 const Crypto = () => {
   const [coinData, setCoinData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
+  const fetchdata = async () => {
+    const response = await fetch(url);
+    const result = await response.json();
+    setCoinData(result);
+  };
+
   useEffect(() => {
-    axios
-      .get(url)
-      .then((res) => {
-        const dataCoin = res.data;
-        setCoinData(dataCoin);
-        setError(null);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsLoading(false);
-      });
+    const interval = setInterval(() => {
+      fetchdata();
+    }, 1000);
+    return () => clearInterval(interval);
   }, []);
 
-  const handelsearch = (searchvalue) => {
-    let value = searchvalue.toLowerCase();
-    const newCoin = coinData.filter((fillcrypto) => {
-      const newCrypto = fillcrypto.name.toLowerCase();
-      return newCrypto.startsWith(value);
-    });
-    setCoinData(newCoin);
-  };
+  // const handelsearch = (searchvalue) => {
+  //   let value = searchvalue.toLowerCase();
+  //   const newCoin = coinData.filter((fillcrypto) => {
+  //     const newCrypto = fillcrypto.name.toLowerCase();
+  //     return newCrypto.startsWith(value);
+  //   });
+  //   setCoinData(newCoin);
+  //
 
   return (
     <>
-      {error && <p>{error} </p>}
-      {isLoading && <p> data is loading</p>}
-      <h1>
-        {" "}
-        <Search onSearch={handelsearch} />{" "}
-      </h1>
-      {coinData && <Coins coinData={coinData} key={coinData.id} />}
+      {coinData.map((coin) => {
+        return (
+          <Coins
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
     </>
   );
 };
